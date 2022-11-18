8c9e06817c31e423734154534f9758010bf0166d
ribells <bcampbel01@risd.edu>
2022-11-18 12:55:20 -0500

    add Sailesh's new level as level 4
    add Sailesh's new level as level 4

---
+++ week8/levels/level_data/bg_barrel.tsx
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="bg_barrel" tilewidth="128" tileheight="128" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="../../graphics/terrain/bg_barrel/1.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="29" y="1" width="71" height="127"/>
+   <object id="2" x="29" y="1" width="71" height="127"/>
+   <object id="3" x="29" y="1" width="71" height="127"/>
+   <object id="4" x="29" y="1" width="71" height="127"/>
+   <object id="5" x="29" y="1" width="71" height="127"/>
+   <object id="6" x="29" y="0" width="71" height="127"/>
+  </objectgroup>
+ </tile>
+</tileset>
