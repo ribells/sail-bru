aaf3f6d666fe2e7bb65ede988ef24b300071da98
ribells <bcampbel01@risd.edu>
2022-11-18 12:53:03 -0500

    move debugged code to week 8
    move debugged code to week 8

--- week 7 debug/levels/level_data/fg_barrel.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.1" name="fg_barrel" tilewidth="128" tileheight="128" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="128" height="128" source="../../graphics/terrain/fg_barrel.png"/>
- </tile>
-</tileset>
